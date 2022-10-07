56fdadf8f74609a0cee1ce9a4dbdb2b16bc713ec
Yuanzhen0520 <yzhang18@scu.edu>
2022-10-06 17:28:12 -0700

    Profile page

--- Routine/Routine/Views/UserPageViews/UserPageView.swift
+++ Routine/Routine/Views/UserPageViews/UserPageView.swift
@@ -7,14 +7,49 @@
 
 import SwiftUI
 
-struct UserPageView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ProfilePicView: View {
+    var body: some View{
+        Text("Hello")
     }
 }
 
-struct UserPageView_Previews: PreviewProvider {
-    static var previews: some View {
-        UserPageView()
+struct UserPageView: View {
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Color(red:240 / 255, green: 240 / 255, blue: 240 / 255).edgesIgnoringSafeArea(.all)
+                VStack(spacing: 20) {
+                    NavigationLink(destination: ProfilePicView()){
+                        Image("me")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                        
+                    }
+                    Text("Name")
+                        .font(.system(size: 20))
+                        .foregroundColor(.black)
+                    Text("Edit")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                    NavigationView{
+                        List{
+                            Text("Points")
+                            Text("Calender")
+                            Text("Achivements")
+                        }
+                    }
+                }.padding(.top, 40)
+            }
+        }.navigationBarTitle("My Profile", displayMode: .inline)
+    }
+    
+    
+    struct UserPageView_Previews: PreviewProvider {
+        static var previews: some View {
+            UserPageView()
+        }
     }
 }
